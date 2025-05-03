44ce43862f7571b8c30dff5b60735b5e01ed8218
mohtishim <mohtishim5@gmail.com>
2025-05-03 22:04:27 +0500

    1

---
+++ app.js
@@ -0,0 +1,24 @@
+// var satisfiedClients = document.getElementById("oneData");
+// var dealDone = document.getElementById("twoData");
+// var successRate = document.getElementById("threeData");
+
+// var satisfied = 0;
+// var deal = 0;
+// var success = 0;
+
+// var countTime = 2000 / 5578;
+
+// setInterval(function setTimer(){
+//     satisfiedClients.innerText = satisfied;
+//     satisfied++
+//     if(satisfied >= 5578){
+//         satisfiedClients.innerText = 5578;
+//         clearInterval()
+//     }
+// }, countTime);
+
+
+
+  
+
+  
